ab58dfa30ea8919fd0a6dcf36441bcb13f737cd2
Kyle Stewart <kylestew@gmail.com>
2024-08-21 00:00:21 +0100

    Send out normals for current output directions

--- sandbox/sketches/circuits_study.ts
+++ sandbox/sketches/circuits_study.ts
@@ -1,12 +1,13 @@
 import { Vec2, Shape, Polygon, Rectangle, Circle, Line, rotate, translate, pointAt } from 'root/geo'
 import { gaussian, pickRandom } from 'root/random'
-import { degreesToRadians } from 'root/math'
+import { add, sub, mulN, normalize, degreesToRadians } from 'root/math'
 import { createCanvas } from '../../dist/canvas'
 
 type Orientation = 0 | 90 | 180 | 270
 interface Port {
     pos: Vec2
     type: 'input' | 'output'
+    normal: Vec2
 }
 interface Symbol {
     pos: Vec2
@@ -37,9 +38,12 @@ class Resistor implements Symbol {
         let a = [-this.size / 2, 0] as Vec2
         let b = [this.size / 2, 0] as Vec2
 
+        const nA = rotate(normalize(a) as Vec2, degreesToRadians(this.orientation)) as Vec2
+        const nB = rotate(normalize(b) as Vec2, degreesToRadians(this.orientation)) as Vec2
+
         return [
-            { pos: this.place(a) as Vec2, type: 'input' },
-            { pos: this.place(b) as Vec2, type: 'output' },
+            { pos: this.place(a) as Vec2, type: 'input', normal: nA },
+            { pos: this.place(b) as Vec2, type: 'output', normal: nB },
         ]
     }
 
@@ -66,14 +70,19 @@ function makeSymbols(): Shape[] {
     const symbol = new Resistor(pos, orientation, size)
 
     // (2) For each port on symbol, start a current moving away from the symbol
+
+    // DEBUG: container, port point and normals
+    const containerRect = new Rectangle(pos, [size, size], { stroke: 'red', weight: 0.005 })
     const ports = symbol.ports().map((port) => {
-        return new Circle(port.pos, 0.02, { fill: 'blue' })
+        return new Circle(port.pos, 0.015, { fill: 'blue' })
+    })
+    const normals = symbol.ports().map((port) => {
+        const bob = add(port.pos, mulN(port.normal, 0.1))
+        return new Line(port.pos, bob, { stroke: 'green', weight: 0.008 })
     })
 
-    const containerRect = new Rectangle(pos, [size, size], { stroke: 'red', weight: 0.005 })
-
     // return symbol
-    return [...symbol.toPolys(), ...ports, containerRect]
+    return [...symbol.toPolys(), ...ports, ...normals, containerRect]
 }
 
 export function sketch(canvas, palette) {
